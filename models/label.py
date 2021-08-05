730286440a88f7e2fc70c778402a57124944543a
hyunwoo <hyunwoo@quriously.com>
2021-08-05 13:36:31 +0900

    Added labels

--- models/label.py
+++ models/label.py
@@ -6,7 +6,6 @@ class Label(Enum):
     ENHANCEMENT = 'enhancement'
     IN_TESTING = 'in-testing'
     TESTED = 'tested'
-    UNRESOLVED = 'unresolved'
     WONTFIX = 'wontfix'
     NEED_WORK = 'need-work'
     DESIGN = 'design'
@@ -19,12 +18,16 @@ class Label(Enum):
     SYSTEM_DESIGN = 'system design'
     UX = 'UX'
     THIRD_PARTY_ISSUE = '3rd party issue'
+    THIRD_PARTY = '3rd party'
     DATA = 'data'
     DUPLICATE = 'duplicate'
     PRIORITY = 'priority'
     BACK_END = 'back-end'
     FRONT_END = 'front-end'
     APP = 'app'
+    FIXED = 'fixed'
+    RESOLVED = 'resolved'
+    UNRESOLVED = 'unresolved'
 
     def __str__(self):
         return self.value
